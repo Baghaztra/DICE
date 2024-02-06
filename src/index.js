58c2cc688760bf4e2618380fdf2176e00a221569
Topan Sidiq <topansidiq28@gmail.com>
2024-02-06 23:36:44 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+<<<<<<< Updated upstream
 import { boardContainer } from "./js/dom/board.dom.js";
 import { Board } from "./js/lib/Board.js";
+=======
+import { boardContainer } from "./js/board.dom.js";
+import { Board } from "./js/Board.js";
+>>>>>>> Stashed changes
 
 const board = new Board(boardContainer);
 board.block();
