35e5957f4d854f7c31ce6134d84da1c2d675327d
Baghaztra <127162951+Baghaztra@users.noreply.github.com>
2024-02-19 17:07:29 +0700

    js-1

---
+++ ShutTheBox/js/script.js
@@ -0,0 +1,12 @@
+const box = document.getElementById('box');
+const playerCount = parseInt(document.getElementById('player').value);
+const player = new Array[playerCount];
+
+for (let i = 0; i < playerCount; i++) {
+    player[i]=  document.getElementById(`player${i+1}`);
+    for (let j = 1; j < 12; j++) {
+        const number = document.createElement('div');
+        number.classList.add(`number-${j}`);
+        player[i].appendChild(number);
+    }
+}
