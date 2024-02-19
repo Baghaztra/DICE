e6abf69832f722a3c961fcf0a753bafd308dcb24
PitokDf <pitokfauzi@gmail.com>
2024-02-19 17:48:10 +0700

    fixed error

--- ShutTheBox/js/script.js
+++ ShutTheBox/js/script.js
@@ -1,14 +1,18 @@
 const box = document.getElementById('box');
-const playerCount = parseInt(document.getElementById('player').value);
-console.log(playerCount);
-const player = new Array[playerCount];
+const playerCount = document.getElementById('player');
 
-for (let i = 0; i < playerCount; i++) {
-    player[i]=  document.getElementById(`player${i+1}`);
-    for (let j = 1; j < 12; j++) {
-        const number = document.createElement('div');
-        number.classList.add(`number-${j}`);
-        player[i].appendChild(number);
-        console.log(player[i]);
-    }
+// const player = new Array[playerCount];
+
+// for (let i = 0; i < playerCount; i++) {
+//     player[i]=  document.getElementById(`player${i+1}`);
+//     for (let j = 1; j < 12; j++) {
+//         const number = document.createElement('div');
+//         number.classList.add(`number-${j}`);
+//         player[i].appendChild(number);
+//         console.log(player[i]);
+//     }
+// }
+
+function start() {
+    console.log(playerCount.value);
 }
