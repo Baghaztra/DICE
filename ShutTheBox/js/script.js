08ae042db23af39718b4962fe2c6e4ff51194b32
Baghaztra <127162951+Baghaztra@users.noreply.github.com>
2024-02-20 10:32:59 +0700

    itu

--- ShutTheBox/js/script.js
+++ ShutTheBox/js/script.js
@@ -1,18 +1,28 @@
 const box = document.getElementById('box');
 const playerCount = document.getElementById('player');
-
-// const player = new Array[playerCount];
-
-// for (let i = 0; i < playerCount; i++) {
-//     player[i]=  document.getElementById(`player${i+1}`);
-//     for (let j = 1; j < 12; j++) {
-//         const number = document.createElement('div');
-//         number.classList.add(`number-${j}`);
-//         player[i].appendChild(number);
-//         console.log(player[i]);
-//     }
-// }
+const playButton = document.getElementById('play');
+let player;
 
 function start() {
-    console.log(playerCount.value);
+    // console.log(typeof(parseInt(playerCount.value)));
+    player = new Array(parseInt(playerCount.value));
+
+    for (let i = 0; i < playerCount.value; i++) {
+        player[i]=  document.getElementById(`player${i+1}`);
+        for (let j = 1; j <= 12; j++) {
+            const number = document.createElement('button');
+            number.classList.add(`number-${j}`);
+            number.innerText = j;
+            player[i].appendChild(number);
+        }
+        const number = document.createElement('button');
+        number.classList.add(`pass`);
+        number.innerText = "pass";
+        player[i].appendChild(number);
+
+        console.log(`Player ${i+1} `, player[i]);
+        player[i].style.display = 'flex';
+    }
+    // console.log(playButton);
+    playButton.style.display = 'none';
 }
