ecffe43f398f08360fee6b813c98e3300e95e7b3
Baghaztra <127162951+Baghaztra@users.noreply.github.com>
2024-02-20 11:40:13 +0700

    jir

--- ShutTheBox/js/script.js
+++ ShutTheBox/js/script.js
@@ -8,21 +8,41 @@ function start() {
     player = new Array(parseInt(playerCount.value));
 
     for (let i = 0; i < playerCount.value; i++) {
+<<<<<<< Updated upstream
         player[i] = document.getElementById(`player${i + 1}`);
+=======
+        player[i]=  {
+            playerElement: document.getElementById(`player${i+1}`)
+        }
+>>>>>>> Stashed changes
         for (let j = 1; j <= 12; j++) {
             const number = document.createElement('button');
             number.classList.add(`number-${j}`);
             number.innerText = j;
-            player[i].appendChild(number);
+            player[i].playerElement.appendChild(number);
         }
         const number = document.createElement('button');
         number.classList.add(`pass`);
         number.innerText = "pass";
-        player[i].appendChild(number);
+        player[i].playerElement.appendChild(number);
 
+<<<<<<< Updated upstream
         console.log(`Player ${i + 1} `, player[i]);
         player[i].style.display = 'flex';
+=======
+        console.log(`Player ${i+1} `, player[i]);
+        player[i].playerElement.style.display = 'flex';
+>>>>>>> Stashed changes
     }
     // console.log(playButton);
     playButton.style.display = 'none';
+}
+
+function gatcha(){
+
+}
+
+
+function choose(number){
+
 }
