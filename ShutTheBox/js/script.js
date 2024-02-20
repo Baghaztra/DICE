6f67b8234f33cb931edbbb133207bf84450db643
Baghaztra <127162951+Baghaztra@users.noreply.github.com>
2024-02-20 11:41:05 +0700

    bla

--- ShutTheBox/js/script.js
+++ ShutTheBox/js/script.js
@@ -8,13 +8,9 @@ function start() {
     player = new Array(parseInt(playerCount.value));
 
     for (let i = 0; i < playerCount.value; i++) {
-<<<<<<< Updated upstream
-        player[i] = document.getElementById(`player${i + 1}`);
-=======
         player[i]=  {
             playerElement: document.getElementById(`player${i+1}`)
         }
->>>>>>> Stashed changes
         for (let j = 1; j <= 12; j++) {
             const number = document.createElement('button');
             number.classList.add(`number-${j}`);
@@ -26,13 +22,8 @@ function start() {
         number.innerText = "pass";
         player[i].playerElement.appendChild(number);
 
-<<<<<<< Updated upstream
-        console.log(`Player ${i + 1} `, player[i]);
-        player[i].style.display = 'flex';
-=======
         console.log(`Player ${i+1} `, player[i]);
         player[i].playerElement.style.display = 'flex';
->>>>>>> Stashed changes
     }
     // console.log(playButton);
     playButton.style.display = 'none';
