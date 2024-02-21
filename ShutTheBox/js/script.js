c2d6b4179f56134732b46b453fad76a7bdcd41af
Baghaztra <127162951+Baghaztra@users.noreply.github.com>
2024-02-21 17:49:10 +0700

    sisa condisi menang

--- ShutTheBox/js/script.js
+++ ShutTheBox/js/script.js
@@ -2,29 +2,34 @@ const box = document.getElementById('box');
 const playerCount = document.getElementById('player');
 const playButton = document.getElementById('play');
 const dice = document.querySelector(".dice");
+let number;
 let player;
 let nowDice = 0;
 
 function start() {
-    player = new Array(parseInt(playerCount.value));
+    player = new Array(parseInt(playerCount.value)); 
+    number = new Array(parseInt(playerCount.value));
     if (playerCount.value > 0 && playerCount.value <= 4) {
         for (let i = 0; i < playerCount.value; i++) {
             player[i]=  {
                 playerElement: document.getElementById(`player${i+1}`),
                 numberPick: 0,
+                pass: true,
                 active: true,
-                pickedNumbers: Array(12).fill(false),
+                pickedNumbers: Array(10).fill(false),
             }
-            for (let j = 1; j <= 12; j++) {
-                const number = document.createElement('button');
-                number.classList.add(`number-${j}`);
-                number.innerText = j;
+            number[i] = new Array(10);
+            for (let j = 1; j <= 10; j++) {
+                const n = document.createElement('button');
+                number[i][j-1] = n
+                number[i][j-1].classList.add(`number-${j}`);
+                number[i][j-1].innerText = j;
         
-                number.addEventListener('click', function() {
-                    choose(j, i, number);
+                number[i][j-1].addEventListener('click', function() {
+                    choose(j, i, number[i][j-1]);
                 });
         
-                player[i].playerElement.appendChild(number);
+                player[i].playerElement.appendChild(number[i][j-1]);
             }
         
             const passButton = document.createElement('button');
@@ -53,31 +58,76 @@ function start() {
 }
 
 function rollTheDice() {
-    let rand1 = Math.floor(Math.random() * 6) + 1;
-    let rand2 = Math.floor(Math.random() * 6) + 1;
-    dice.innerHTML = `<i class="dice bi bi-dice-${rand1}"></i> <i class="dice bi bi-dice-${rand2}"></i>`;
-    nowDice = rand1 + rand2;
-    console.log("Pilih nomor berjumlah ", nowDice);
+    if (someoneWon()) {
+        // Tampilkan sebuah anu, yang nunjukin siapa yang menang, + tombol rematch
+
+    } else if (done()) {
+        for (let i = 0; i < playerCount.value; i++) {
+            player[i].numberPick = 0;
+            console.log(`[Holding] Player ${i+1}: ${player[i].numberPick}`);
+        }
+        for (let i = 0; i < parseInt(playerCount.value); i++) {
+            for (let j = 0; j < 10; j++) {
+                if (number[i][j].classList.contains('picked')) {
+                    number[i][j].classList.add('picked-hold');
+                    number[i][j].classList.remove('picked');
+                    number[i][j].removeEventListener('click', function() {
+                        choose(j, i, number[i][j-1]);
+                    });
+                }
+            }
+        }
+        let rand1 = Math.floor(Math.random() * 6) + 1;
+        let rand2 = Math.floor(Math.random() * 6) + 1;
+        dice.innerHTML = `<i class="dice bi bi-dice-${rand1}"></i> <i class="dice bi bi-dice-${rand2}"></i>`;
+        nowDice = rand1 + rand2;
+        console.log("[Rolling the dice] Pilih nomor berjumlah ", nowDice);
+    } else{
+        console.log("[Rolling the dice] Belum semua player siap ", player);
+    }
 }
 
+function done(){
+    for (let i = 0; i < playerCount.value; i++) {
+        if(player[i].numberPick != nowDice){
+            console.log(`[Belum memenuhi] Player ${i+1} belum siap`);
+            // console.log(player[i].numberPick,' != ', nowDice);
+            return false;
+        }
+    }
+    return true;
+}
+
+function someoneWon() {
+    const sudahSemua = Array(10).fill(true);
+    for (let i = 0; i < playerCount.value; i++) {
+        if(player[i].pickedNumbers == sudahSemua){
+            return true;
+        }
+    }
+    return false;
+}
 
 function choose(buttonNum, playerNum, numberElement) {
-    if (!player[playerNum].pickedNumbers[buttonNum - 1]) {
-        player[playerNum].numberPick += buttonNum;
-        console.log(`Player${playerNum + 1} picked ${buttonNum}`);
-        console.log(`Player${playerNum + 1} number picked: ${player[playerNum].numberPick}`);
-        player[playerNum].pickedNumbers[buttonNum - 1] = true;
-        numberElement.classList.add('picked');
-    }else{
-        player[playerNum].numberPick -= buttonNum;
-        console.log(`Player${playerNum + 1} unpicked ${buttonNum}`);
-        console.log(`Player${playerNum + 1} number picked: ${player[playerNum].numberPick}`);
-        player[playerNum].pickedNumbers[buttonNum - 1] = false;
-        numberElement.classList.remove('picked');
+    if (!numberElement.classList.contains('picked-hold')) {
+        if (!player[playerNum].pickedNumbers[buttonNum - 1]) {
+            player[playerNum].numberPick += buttonNum;
+            console.log(`[Choosing] Player${playerNum + 1} picked ${buttonNum}`);
+            console.log(`[Holding] Player${playerNum + 1} number holded: ${player[playerNum].numberPick}`);
+            player[playerNum].pickedNumbers[buttonNum - 1] = true;
+            numberElement.classList.add('picked');
+        }else{
+            player[playerNum].numberPick -= buttonNum;
+            console.log(`[Choosing] Player${playerNum + 1} unpicked ${buttonNum}`);
+            console.log(`[Holding] Player${playerNum + 1} number holded: ${player[playerNum].numberPick}`);
+            player[playerNum].pickedNumbers[buttonNum - 1] = false;
+            numberElement.classList.remove('picked');
+        }
     }
 }
 
 function pass(playerNum){
     player[playerNum].numberPick = 0;
-    console.log(`Player${playerNum+1} pass`);
+    player[playerNum].pass = true;
+    console.log(`[Choosing] Player${playerNum+1} pass`);
 }
